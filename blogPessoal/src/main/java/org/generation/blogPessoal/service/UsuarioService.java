ed429a3f5db5d2a9a172a87e08342ec9e8e87f71
Claudia Yapuchura <claudiasaire213@gmail.com>
2020-08-07 19:26:35 -0300

    Adicionar classe BasicSecurityConfig no Package-Segurança

--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -35,7 +35,7 @@ public class UsuarioService {
 			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
 				
 				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
-				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ACII")));
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodeAuth);
 				
 				user.get().setToken(authHeader);
